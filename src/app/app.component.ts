f9dbc8b54c77c200863f91a6855e4ff2ad5962a6
arthurgeay <arthurgeay.contact@gmail.com>
2019-12-04 19:40:01 +0100

    Mise en place de l'authentification avec Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book-manage';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCXSOUXdyfaBWqqhFK6QHfi7ENGJjTzvfA",
+      authDomain: "book-manage-d6a0d.firebaseapp.com",
+      databaseURL: "https://book-manage-d6a0d.firebaseio.com",
+      projectId: "book-manage-d6a0d",
+      storageBucket: "book-manage-d6a0d.appspot.com",
+      messagingSenderId: "120392306952",
+      appId: "1:120392306952:web:647f1aaf694e27fdc59cbd"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
